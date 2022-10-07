01071abaa90f5894bd2e1b2e6887c6b4260468de
Ashwin Prasad <ashwin@onpar.in>
2022-10-08 01:59:36 +0530

    logs

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -23,5 +23,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const data = await findAllUsers();
+  console.log({ data: req.body });
   res.status(200).json({ data: req.body });
 }
