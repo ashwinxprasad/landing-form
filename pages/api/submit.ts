dbf3258d9a2ceb4ff375259f9a798c0b36081213
Ashwin Prasad <ashwin@onpar.in>
2023-01-11 21:57:35 +0530

    added mdb

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../lib/mongodb";
 import airtable from "airtable";
 import moment from "moment";
 import NextCors from "nextjs-cors";
 import validator from "validator";
+import { makeUserCollection } from "../../lib/collections/user";
+import { Collection } from "mongoose";
 
 const base = airtable.base(process.env.BASE_ID as string);
 const table = base(process.env.TABLE_NAME as string);
@@ -25,39 +28,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
   await NextCors(req, res, {
     // Options
-    methods: ['POST', "OPTION"],
-    origin: '*',
+    methods: ["POST", "OPTION"],
+    origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
   const email = req.body.data["user_email"];
-  console.log({ email })
-  console.log({ validate: validator.isEmail(email) });
 
   if (!validator.isEmail(email)) {
-    console.log("Error validating", email)
-    return res.status(400).json({ oops: "error" })
+    console.log("Error validating", email);
+    return res.status(400).json({ oops: "error" });
   }
 
-
-  const data = await findAllUsers();
-  console.log({ data })
-  if (!email)
-    if (!data.includes(email)) {
-      await table.create([
-        {
-          fields: {
-            Email: email,
-            Date: moment().format("YYYY-MM-DD"),
-            Name: "Webflow Form User",
-          },
-        },
-      ]);
-      console.log("Record added");
-    } else {
-      console.log("Redundant email");
-    }
+  const userCollection: Collection = await makeUserCollection();
+  userCollection.insertOne({
+    email,
+    createdAt: new Date(),
+    level: 0,
+  });
   res.status(200).json({ data: req.body });
 }
